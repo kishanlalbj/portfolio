3c74c3361e1053787a43e5dee6ff934b5642f280
Kishanlal B J <kishanlalbj@gmail.com>
2025-01-03 18:45:44 +0530

    chore(docker): local docker dev setups done

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    watch: {
+      usePolling: true
+    }
+  },
   base: "./",
   resolve: {
     alias: {
