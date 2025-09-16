223a84a9e612634f6f9f69fb877a5d35bf3f7552
Kishanlal B J <kishanlalbj@gmail.com>
2025-09-16 17:38:38 +0530

    chore(speed-insights): enabled vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext.jsx";
 
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </ThemeContextProvider>
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
