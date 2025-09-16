7a6b97d5abd732b2ec5e9f911541e102dc353f63
Kishanlal B J <kishanlalbj@gmail.com>
2025-09-16 17:46:52 +0530

    chore: removed speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext.jsx";
 
@@ -12,6 +11,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </ThemeContextProvider>
     <Analytics />
-    <SpeedInsights />
   </React.StrictMode>
 );
