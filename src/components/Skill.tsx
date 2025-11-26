eaaf0b1d0c384e5cb25097edc9ddf4049ffe4147
Kishanlal B J <kishanlalbj@gmail.com>
2025-11-26 19:43:55 +0530

    chrome: migrate jsx to tsx

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,10 +1,11 @@
 import { motion } from "motion/react";
+import { Skill as SkillType } from "../types";
 
-const Skill = ({ img, name }) => {
+const Skill = ({ logo, name }: SkillType) => {
   return (
     <motion.li className="border border-gray-800 p-4 rounded-md inline-flex items-center justify-center gap-4 hover:shadow-sm shadow-primary w-full">
-      <img src={img} alt={name} width={"36px"}></img>
-      <p>{name}</p>
+      <img src={logo} alt={name} width={"36px"}></img>
+      <span className="inline-block">{name}</span>
     </motion.li>
   );
 };
